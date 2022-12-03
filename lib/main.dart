52bfc53f47cd931e8de12e3b54edf1b8fea9b4c8
AracelyCondoriTipola <aracely96.ac>
2022-12-03 13:05:28 -0400

    modificando yml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+//import 'package:frontend_app/ul/home_page.dart';
+import 'package:frontend_app/ul/login_page.dart';
+//import 'package:frontend_app/ul/order_page.dart';
+//import 'package:frontend_app/ul/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:frontend_app/ul/login_page.dart';
+
 import 'cubit/app_cubit.dart';
 
 void main() {
@@ -13,58 +17,57 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Delivery',
-      theme: ThemeData(
-        // This is the theme of your application.
-
-        primarySwatch: Colors.blue,
-      ), //themeData
-      home: MultiBlocProvider(providers: [
-        // registro
-        BlocProvider(create: (context) => AppCubit()),
-        //  BlocProvider(create: (context) => SingInCubit()),
-        //  BlocProvider(create: (context) => MakeOrderCubit()),
-      ], child: const MyHomePage()),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Delivery"),
-        ),
-        body: BlocBuilder<AppCubit, AppState>(
-          builder: (BuildContext context, state) {
-            if (!state.isLoading) {
-              return LoginPage();
-            } else if (state.isLoading) {
-              //return const Center(child: CircularProgressIndicador());
-            } else if (state.isUserLoggedIn) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Bienvenido ${state.loginResponseDto!.firstName}"),
-                    ElevatedButton(
-                        //boton
-                        onPressed: () {
-                          BlocProvider.of<AppCubit>(context).logout();
-                        },
-                        child: const Text("Logout")) //ElevatedButton
-                  ],
-                ), //Column
-              ); //Center
-            } else {
-              return const Center(child: Text("Error"));
-            }
+    return BlocProvider(
+        create: (context) => AppCubit(),
+        child: MaterialApp(
+          title: 'Pedidos Ya!',
+          theme: ThemeData(
+            // This is the theme of your application.
+            primarySwatch: Colors.purple,
+          ),
+          initialRoute: "/",
+          routes: {
+            "/": (context) => const Login_Page(),
+            "/home": (context) => const HomePage(),
+            "/newOrder": (context) => const OrderPage(),
+            "/register": (context) => const RegisterPage(),
           },
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        ); //Scaffold
+        ));
   }
 }
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Pedidos Ya!"),
+//         ),
+//         body: BlocBuilder<AppCubit, AppState>(
+//           builder: (BuildContext context, state) {
+//             if (!state.isUserLoggedIn) {
+//               return LoginPage();
+//             } else if (state.isUserLoggedIn) {
+//               return Center(
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Text("Bienvenido ${state.loginResponseDto!.firstName}"),
+//                     ElevatedButton(
+//                         onPressed: () {
+//                           BlocProvider.of<AppCubit>(context).logout();
+//                         },
+//                         child: const Text("Logout"))
+//                   ],
+//                 ),
+//               );
+//             } else {
+//               return const Center(child: Text("Error"));
+//             }
+//           },
+//         ) // This trailing comma makes auto-formatting nicer for build methods.
+//         );
+//   }
+// }
